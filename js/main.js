be336b7c2be6ee93117c11d319ddcd0af4070c70
world666 <world666.kirillov@mail.ru>
2015-10-18 18:43:41 +0200

    angularjs support

---
+++ js/main.js
@@ -0,0 +1,18 @@
+paradoxAJ = angular.module('paradoxApp', []);
+
+$( document ).ready(function() 
+{
+	console.log( "ready!" );
+});
+
+
+paradoxAJ.controller('MainCtrl', function($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});

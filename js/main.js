71a9c8173db40030a4aa18755c18f887bedd83fb
world666 <world666.kirillov@mail.ru>
2015-10-24 23:51:34 +0200

    JSON request to server

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 paradoxAJ = angular.module('paradoxApp', []);
 
+var markers = [{ "position": "128.3657142857143", "markerPosition": "7" },
+               { "position": "235.1944023323615", "markerPosition": "19" },
+               { "position": "42.5978231292517", "markerPosition": "-3" }];
+
+
 $( document ).ready(function() 
 {
 	console.log( "ready!" );
+	$.ajax({
+		type: 'POST',
+		url: 'http://192.168.1.11:5000',
+		data: JSON.stringify({ Markers: markers }),
+		dataType: 'json',
+		success: function (data) {
+			if (data.response == 'captcha') {
+				alert('captcha');
+			} else if (data.response == 'success') {
+				alert('success');
+			} else {
+				alert('sorry there was an error');
+			}
+		},
+		error: function (result) {
+			alert("Error");
+		}
+	});
 });
 
 

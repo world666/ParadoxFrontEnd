7287e604b80c1b31ac7f79f6466f046999feb9c0
world666 <world666.kirillov@mail.ru>
2016-01-10 20:25:03 +0100

    angular js input controll

--- js/main.js
+++ js/main.js
@@ -8,30 +8,48 @@ var markers = [{ "position": "128.3657142857143", "markerPosition": "7" },
 $( document ).ready(function() 
 {
 	console.log( "ready!" );
-	console.log( "send json request: " + JSON.stringify({ Markers: markers }));
+	
+});
+
+function SendQuery(query, callback)
+{
+	console.log( "send json request: " + JSON.stringify(query));
 	$.ajax({
 		type: 'POST',
-		url: 'http://192.168.1.11:5000',
-		data: JSON.stringify({ Markers: markers }),
+		url: 'http://192.168.0.50:5000',
+		data: JSON.stringify(callback),
 		dataType: 'json',
 		success: function (data) {
 			console.log( "Server answer: " + JSON.stringify(data));
+			callback(data);
 		},
 		error: function (result) {
 			console.log( "Server answer: " + result.statusText);
 			console.log( "Ready State: " + result.readyState);
 		}
 	});
-});
+}
 
 
 paradoxAJ.controller('MainCtrl', function($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+  /*SendQuery({"name":"andrey"}, function(answer)
+  {
+	$scope.phone = answer;
+	$scope.$apply()
+  });*/
+  
+  var _name = 'Brian';
+  $scope.user = {
+    name: function(newName) {
+	 if(arguments.length)
+	 {
+	   SendQuery({"name":newName}, function(answer)
+	   {
+	     _name = answer.name;
+	     $scope.$apply();
+	   });
+	 }
+     return _name;
+    }
+  };
 });
